81a88c2172c19f26451b8bf3a254696207203091
Jack Dent <jack@jackdent.co.uk>
2014-04-19 22:42:03 +0100

    Added clients to root class

--- lib/justgiving.rb
+++ lib/justgiving.rb
@@ -1,3 +1,9 @@
 module JustGiving
 
+  require_relative "justgiving/charity"
+  require_relative "justgiving/country"
+  require_relative "justgiving/currency"
+  require_relative "justgiving/donation"
+  require_relative "justgiving/search"
+
 end
