4834529f34dd4a306b4e6097f2b1ca3a1feca889
Jack Dent <jack@jackdent.co.uk>
2014-04-21 09:58:00 +0100

    Removed addressable gem to rely on Faraday hash handling

--- justgiving.gemspec
+++ justgiving.gemspec
@@ -23,5 +23,4 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency "faraday", "~> 0.9"
   spec.add_runtime_dependency "faraday_middleware", "~> 0.9"
-  spec.add_runtime_dependency "addressable", "~> 2.3"
 end
--- lib/justgiving/client.rb
+++ lib/justgiving/client.rb
@@ -1,6 +1,5 @@
 require "faraday"
 require "faraday_middleware"
-require "addressable/uri"
 require_relative "error"
 
 module JustGiving
@@ -10,33 +9,28 @@ module JustGiving
       @token = token
       @base_url = "https://api.justgiving.com/#{@token}/v1/"
       @connection_defaults = {
-          url: @base_url,
-          headers: {
-            'User-Agent' => 'Ruby REST client',
-            'Content-Type' => 'application/json'
-          }
+        url: @base_url,
+        headers: {
+          'User-Agent' => 'Ruby REST client',
+          'Content-Type' => 'application/json'
         }
+      }
+      @connection = Faraday.new(@connection_defaults) do |connection|
+        connection.response :json, :content_type => /\bjson$/
+        connection.response :logger
+        connection.adapter   Faraday.default_adapter
+        connection.use       Error::RaiseError
+      end
     end
 
     protected
 
-      def get(path, query_hash=nil)
-        @connection ||= Faraday.new(@connection_defaults) do |connection|
-          connection.response :json, :content_type => /\bjson$/
-          connection.response :logger
-          connection.adapter   Faraday.default_adapter
-          connection.use       Error::RaiseError
+      def get(path, query=nil)
+        response = @connection.get do |request|
+          request.url path
+          request.params.merge! query if query
         end
-
-        path += '?' + query_string_from_hash(query_hash) if query_hash
-        response = @connection.get path
         response.body
-      end
-
-      def query_string_from_hash(query_hash)
-        uri = Addressable::URI.new
-        uri.query_values = query_hash
-        uri.query
       end
 
   end
